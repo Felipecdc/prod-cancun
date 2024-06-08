a7ccc4eff733e6e92966daba88bd65e361b64a1f
Felipe Castro <felipecdc09@gmail.com>
2024-06-08 17:55:29 -0300

    feat: add toastcontainer on app

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { router } from "./App.tsx";
 import "./index.scss";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer position="bottom-center" theme="dark" />
   </React.StrictMode>
 );
